7148a0f30d2265b61eb48a0eea2a49c0e8a32efa
R-RANJITHRAJA <nitranjith2019@gmail.com>
2024-09-28 12:09:25 +0530

    styling

---
+++ Foodie/src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+        <div className="header">
+            <div className="header-contents">
+                <h1>Order your favourite food here</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, cumque.</p>
+                <button>ViewMenu</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
